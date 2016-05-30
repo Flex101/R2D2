070098da162acd1918317e349b0411d59d841bd4
Flex101 <msgoodliffe@gmail.com>
2016-05-30 15:49:14 +0100

    Added StopWatch and Timer to RealTime

--- ControlSystem/Tools/RealTime.cpp
+++ ControlSystem/Tools/RealTime.cpp
@@ -1,8 +1,9 @@
 #include "RealTime.h"
 #include <unistd.h>		// for sleep and usleep
-#include <time.h>		// for nanosleep
 
 using namespace R2D2;
+
+/***** RealTime *****/
 
 void RealTime::sleepNano(unsigned int nanoSecs)
 {
@@ -26,4 +27,116 @@ void RealTime::sleepMilli(unsigned int milliSecs)
 void RealTime::sleepSec(unsigned int seconds)
 {
 	sleep(seconds);
+}
+
+timespec RealTime::timeDiff(const timespec& start, const timespec& end)
+{
+	timespec diff;
+
+	if ((end.tv_nsec - start.tv_nsec) < 0)
+	{
+		diff.tv_sec = end.tv_sec - start.tv_sec - 1;
+		diff.tv_nsec = end.tv_nsec - start.tv_nsec + 1000000000;
+	}
+	else
+	{
+		diff.tv_sec = end.tv_sec - start.tv_sec;
+		diff.tv_nsec = end.tv_nsec - start.tv_nsec;
+	}
+
+	return diff;
+}
+
+bool RealTime::isAfter(const timespec& time1, const timespec& time2)
+{
+	if (time1.tv_sec > time2.tv_sec) return true;
+	if (time1.tv_sec < time2.tv_sec) return false;
+	if (time1.tv_nsec > time2.tv_nsec) return true;
+
+	return false;
+}
+
+/***** StopWatch *****/
+
+StopWatch::StopWatch()
+{
+	running = false;
+}
+
+void StopWatch::start()
+{
+	clock_gettime(CLOCK_REALTIME, &startTime);
+	running = true;
+}
+
+void StopWatch::stop()
+{
+	running = false;
+}
+
+void StopWatch::restart()
+{
+	clock_gettime(CLOCK_REALTIME, &startTime);
+}
+
+timespec StopWatch::elapsed()
+{
+	clock_gettime(CLOCK_REALTIME, &now);
+	return RealTime::timeDiff(startTime, now);
+}
+
+/***** Timer *****/
+
+Timer::Timer()
+{
+
+}
+
+void Timer::start(timespec _duration)
+{
+	duration = _duration;
+	stopwatch.start();
+}
+
+void Timer::stop()
+{
+	stopwatch.stop();
+}
+
+void Timer::restart()
+{
+	stopwatch.restart();
+}
+
+void Timer::startSec(unsigned int seconds)
+{
+	duration.tv_sec = seconds;
+	duration.tv_nsec = 0;
+	stopwatch.start();
+}
+
+void Timer::startMilli(unsigned int milliSecs)
+{
+	duration.tv_sec = 0;
+	duration.tv_nsec = milliSecs * 1000000;
+	stopwatch.start();
+}
+
+void Timer::startMicro(unsigned int microSecs)
+{
+	duration.tv_sec = 0;
+	duration.tv_nsec = microSecs * 1000;
+	stopwatch.start();
+}
+
+void Timer::startNano(unsigned int nanoSecs)
+{
+	duration.tv_sec = 0;
+	duration.tv_nsec = nanoSecs;
+	stopwatch.start();
+}
+
+bool Timer::hasElapsed()
+{
+	return RealTime::isAfter(stopwatch.elapsed(), duration);
 }
--- ControlSystem/Tools/RealTime.h
+++ ControlSystem/Tools/RealTime.h
@@ -1,7 +1,11 @@
 #ifndef TIME_H
 #define TIME_H
 
+#include <time.h>		// for timespec and nanosleep()
+
 namespace R2D2 {
+
+/***** RealTime *****/
 
 class RealTime
 {
@@ -10,7 +14,55 @@ public:
 	static void sleepMicro(unsigned int microSecs);
 	static void sleepMilli(unsigned int milliSecs);
 	static void sleepSec(unsigned int seconds);
+
+	static timespec timeDiff(const timespec& start, const timespec& end);
+	static bool isAfter(const timespec& time1, const timespec& time2);
 };
+
+/***** StopWatch *****/
+
+class StopWatch
+{
+public:
+	explicit StopWatch();
+	virtual ~StopWatch() {}
+
+	void start();
+	void stop();
+	void restart();
+
+	timespec elapsed();
+
+protected:
+	bool running;
+	timespec startTime;
+	timespec now;
+};
+
+/***** Timer *****/
+
+class Timer
+{
+public:
+	explicit Timer();
+	virtual ~Timer() {}
+
+	void start(timespec duration);
+	void stop();
+	void restart();
+
+	void startSec(unsigned int seconds);
+	void startMilli(unsigned int milliSecs);
+	void startMicro(unsigned int microSecs);
+	void startNano(unsigned int nanoSecs);
+
+	bool hasElapsed();
+
+protected:
+	StopWatch stopwatch;
+	timespec duration;
+};
+
 
 } // namespace R2D2
 
