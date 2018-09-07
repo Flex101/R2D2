7e66fd91fbd15a44cd3dc1caeeeeafe04a643a5f
Flex101 <matt_goodliffe@hotmail.com>
2018-09-07 21:57:39 +0100

    StringEx startsWith BUG FIX

--- ControlSystem/Tools/StringEx.cpp
+++ ControlSystem/Tools/StringEx.cpp
@@ -5,7 +5,7 @@ using namespace R2D2;
 
 bool StringEx::startsWith(std::string src, std::string prefix)
 {
-	return src.substr(prefix.length()) == prefix;
+	return src.substr(0, prefix.length()) == prefix;
 }
 
 bool StringEx::endsWith(std::string src, std::string suffix)
